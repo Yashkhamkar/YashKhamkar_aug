fc4d932debb49050afddb712821828449ebb24bc
Yashkhamkar <yashkhamkar1100@gmail.com>
2025-08-18 22:52:38 +0530

    commit

---
+++ server/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from sqlmodel import SQLModel
+from sqlalchemy import text
+from server.config.db import engine
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+
+
+@app.get("/ping")
+def ping():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT version();"))
+        version = result.fetchone()
+    return {"postgres_version": version[0]}
